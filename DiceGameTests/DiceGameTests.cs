eebd7daf91371fb13e69b58a5506c069be6b4bd3
oigiselle <gisellesalvadorx@gmail.com>
2022-08-07 18:32:54 -0300

    4 tests to 2 arguments dice were created. All passing.

--- DiceGameTests/DiceGameTests.cs
+++ DiceGameTests/DiceGameTests.cs
@@ -91,4 +91,80 @@ namespace DiceGameTests
 
 
 
+
+        [TestMethod]
+        [DataRow(10)]
+        [DataRow(45)]
+        [DataRow(99)]
+        [DataRow(2)]
+        public void TwoArgumentDieHasLessThanOrISEqualTheArgumentNumberOfSides(int sides)
+        {
+            var randomNumber = dice.Dice(sides, 1);
+
+            int result = Int32.Parse(randomNumber);
+
+
+            Assert.IsTrue(result <= sides);
+        }
+
+        [TestMethod]
+        [DataRow(6, 5)]
+        [DataRow(10, 20)]
+        [DataRow(5, 2)]
+        [DataRow(3, 40)]
+        public void TwoArgumentDieListResultHasTheExactLengthOfArgumentNumberRolls(int sides, int rolls)
+        {
+            string initialize = dice.Dice(sides, rolls);
+
+
+            string[] elements = initialize.Split(' ');
+
+            int result = elements.Length;
+
+            Assert.AreEqual(rolls, result);
+
+        }
+
+        [TestMethod]
+        [DataRow(6, 0)]
+        [DataRow(10, -1)]
+        [DataRow(5, -5)]
+        [DataRow(3, -40)]
+        public void TwoArgumentDieNumberOfRolIfIsZeroOrNegativeReturnError(int sides, int rolls)
+        {
+            string result = dice.Dice(sides, rolls);
+
+            string expected = "Error";
+
+            Assert.AreEqual(expected, result);
+
+        }
+
+        [TestMethod]
+        [DataRow(1, 1)]
+        [DataRow(5, 1)]
+        [DataRow(20, 1)]
+
+        public void TwoArgumentDieIsBetween1andNumberOfSides(int sides, int rolls)
+        {
+            string initialize = dice.Dice(sides, rolls);
+
+            string[] elements = initialize.Split(' ');
+
+            string first = elements.FirstOrDefault();
+
+            int result = Int32.Parse(first);
+
+            bool isGreaterOrEqualToZero = result > 0;
+            bool isLessOrEqualToSix = result <= sides;
+
+            Assert.IsTrue(isLessOrEqualToSix && isGreaterOrEqualToZero);
+        }
+
+
+
+    }
+
+
+
 }
