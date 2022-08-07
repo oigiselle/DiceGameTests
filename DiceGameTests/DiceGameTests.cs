5248cfbe38d93a933dc48278501f912d28a80ed1
oigiselle <gisellesalvadorx@gmail.com>
2022-08-07 18:30:21 -0300

    Is not null and 4 tests for Zero argument tests were created

--- DiceGameTests/DiceGameTests.cs
+++ DiceGameTests/DiceGameTests.cs
@@ -1,3 +1,57 @@
 using DiceGameTests;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace DiceGameTests
+{
+
+
+    [TestClass]
+    public class DiceGameTests
+    {
+        DiceGame dice = new DiceGame();
+
+        [TestMethod]
+        public void DiceNullTest()
+        {
+
+            Assert.IsNotNull(dice);
+        }
+
+        [TestMethod]
+        public void ResultOfZeroArgumentDieIsNotNegative()
+        {
+            int result = dice.Dice();
+
+            Assert.IsTrue(result > 0);
+        }
+
+
+        [TestMethod]
+        public void ZeroArgumentDieHasLessThanOrIsEqualToSixSides()
+        {
+            int result = dice.Dice();
+
+            Assert.IsTrue(result <= 6);
+        }
+
+        [TestMethod]
+        public void ZeroArgumentDieIsBetween1and6()
+        {
+            int initialize = dice.Dice();
+
+            bool isGreaterOrEqualToZero = initialize > 0;
+            bool isLessOrEqualToSix = initialize <= 6;
+
+            Assert.IsTrue(isLessOrEqualToSix && isGreaterOrEqualToZero);
+        }
+
+
+        [TestMethod]
+        public void ZeroArgumentDieResultIsNotZero()
+        {
+            int result = dice.Dice();
+
+            Assert.IsTrue(result != 0);
+        }
+
 }
